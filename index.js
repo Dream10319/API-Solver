da96626bc5ff85d8e217f226ef24d22a422f7611
Vigil1Light <zerry319.wit@gmail.com>
2024-05-16 14:49:24 +0800

    update for all api

--- index.js
+++ index.js
@@ -6,24 +6,45 @@ const app = express();
 app.use(express.json());
 const server = http.createServer(app);
 
-app.use("/v1/solver", (req, res) => {
-    console.log(Date.now())
-    fs.readFile('temp.json', 'utf8', function (err, data) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            // Parse the JSON data
-            const jsonData = JSON.parse(data);
+app.use("/v1/baemin/solver", (req, res) => {
+    console.log('baemin', Date.now())
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
-            // Set the response header
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+    // Send the JSON data as response
+    // res.end(JSON.stringify(jsonData));
+    res.end("5NcSD/b68BGSy+oL0soV4ZfI1zMjFb66m5fAaVhfwkoTQaWle6aVJ0h6YEGzx32RyduVWvMaoUkme8EUqQ3FecI/trBds6xf1QBzt093Nl0O7l2PIYQd0JOCOYxdOSTB0faFO/vDXi+HhNnX/bvee6ed4MCDE7vgiJAWpadWqRH4WLZ9XWvJMK+SiEcyumPV")
 
-            // Send the JSON data as response
-            // res.end(JSON.stringify(jsonData));
-            res.end("5NcSD/b68BGSy+oL0soV4ZfI1zMjFb66m5fAaVhfwkoTQaWle6aVJ0h6YEGzx32RyduVWvMaoUkme8EUqQ3FecI/trBds6xf1QBzt093Nl0O7l2PIYQd0JOCOYxdOSTB0faFO/vDXi+HhNnX/bvee6ed4MCDE7vgiJAWpadWqRGp4J2Xwr8wB4zhwlREYlTt")
-        }
-    });
+})
+
+app.use("/v1/yogiyo/solver", (req, res) => {
+    console.log('yogiyo', Date.now())
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // Send the JSON data as response
+    // res.end(JSON.stringify(jsonData));
+    res.end("https://api.zenrows.com/v1/?apikey=3a14ceddf104f28fdc51f8d07417a31ca2c42357&url=")
+})
+
+app.use("/v1/cardsales/solver", (req, res) => {
+    console.log('creditsales', Date.now())
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // Send the JSON data as response
+    // res.end(JSON.stringify(jsonData));
+    res.end("YTBmZDNlYzQtMGQ0ZC00NzljLWE1NDktZjQwYjE4NmNlMDE1OjdjYzIxOTY4LTJiNzAtNDhlMy05MzExLTQwMjcxYTg2YzA2OA==")
+})
+
+app.use("/v1/tossabledigits/token", (req, res) => {
+    console.log('tossabledigits', Date.now())
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // Send the JSON data as response
+    // res.end(JSON.stringify(jsonData));
+    res.end("MAV3-19KA-JE1B-A0P3")
 })
 
 server.listen(5001, () => {
