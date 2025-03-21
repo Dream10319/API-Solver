6e8a9d6d16facb46fd323dfbbae6e83039f40047
DreamHigh <lovetom.pro@gmail.com>
2025-03-21 11:13:21 +0800

    update api

--- index.js
+++ index.js
@@ -110,8 +110,10 @@ app.use('/v1/review', (req, res) => {
         return res.status(400).json({ error: "Missing parameters" });
     }
 
-    const sql = `SELECT owner_comment FROM reviews WHERE rating LIKE ? AND menu_name LIKE ? ORDER BY RAND() LIMIT 1`;
-    const values = [`%${rating}%`, `%${menu}%`];
+    const id = Math.floor(Math.random() * 872700) + 1;
+    console.log(id);
+    const sql = `SELECT owner_comment FROM reviews WHERE rating LIKE ? AND menu_name LIKE ? AND id > ? LIMIT 1`;
+    const values = [`%${rating}%`, `%${menu}%`, `%${id}%`];
 
     // Log the final SQL query by replacing placeholders
     const finalQuery = sql.replace(/\?/g, () => `'${values.shift()}'`);
